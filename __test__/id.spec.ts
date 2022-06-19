e4b953bef2c0071da03391d305cb2464c7957144
guzhongren <guzhongren@live.cn>
2022-06-19 09:21:46 +0800

    feat: Sad path for UID

--- __test__/id.spec.ts
+++ __test__/id.spec.ts
@@ -6,6 +6,7 @@ import { uuidV4URL } from "../src/config";
 
 import app from "../src/app";
 import { FIXTURE_UUID, PATH_UID } from "./fixture";
+import exp from "constants";
 
 describe("uID endpoint", () => {
   let mockServer: SetupServerApi;
@@ -32,5 +33,22 @@ describe("uID endpoint", () => {
 
     expect(response.statusCode).toEqual(httpStatus.CREATED);
     expect(response.body.id).toEqual(FIXTURE_UUID);
+  });
+
+  it("should return 500 given low level is down", async () => {
+    mockServer = setupServer(
+      rest.get(uuidV4URL, (req, res, ctx) => {
+        return res(ctx.status(httpStatus.INTERNAL_SERVER_ERROR));
+      })
+    );
+    mockServer.listen();
+
+    const response = await request.get(PATH_UID);
+
+    expect(response.statusCode).toEqual(httpStatus.INTERNAL_SERVER_ERROR);
+    expect(response.body.errors).toHaveLength(1);
+    expect(response.body.errors[0]).toEqual(
+      httpStatus[httpStatus.INTERNAL_SERVER_ERROR]
+    );
   });
 });
--- src/routers/uid.ts
+++ src/routers/uid.ts
@@ -1,5 +1,6 @@
 import Router from "@koa/router";
 import axios from "axios";
+import httpStatus from "http-status";
 
 import { uuidV4URL } from "../config";
 const uIDRouter = new Router();
@@ -7,11 +8,19 @@ const uIDRouter = new Router();
 type UUIDResponse = [String];
 
 uIDRouter.get("/uid", async (ctx, _next) => {
-  const response = await axios.get(uuidV4URL);
-  const result = (await response.data) as UUIDResponse;
+  try {
+    const response = await axios.get(uuidV4URL);
+    const result = (await response.data) as UUIDResponse;
 
-  ctx.status = 201;
-  ctx.body = { id: result[0] };
+    ctx.status = httpStatus.CREATED;
+    return (ctx.body = { id: result[0] });
+  } catch (error) {
+    console.error(`Failed to request uuid, error: ${error}`);
+    ctx.status = httpStatus.INTERNAL_SERVER_ERROR;
+    return (ctx.body = {
+      errors: [httpStatus[httpStatus.INTERNAL_SERVER_ERROR]],
+    });
+  }
 });
 
 export default uIDRouter;
